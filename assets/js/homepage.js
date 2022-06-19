0dd18589328806151f8b9386783e56ae00085cfe
olliedirker <olliedirker@gmail.com>
2022-06-19 17:28:15 -0500

    the language buttons dont work

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,86 +1,124 @@
-var getUserRepos = function (user) {
-
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
-    //make url request
-    fetch(apiUrl)
-        .then(function (response) {
-            //request succesful
-            if (response.ok) {
-                response.json().then(function (data) {
-                    displayRepos(data, user);
-                });
-            } else {
-                alert("Error: GitHub User Not Found");
-            }
-        })
-        //at the end of a then function?
-        .catch(function (error) {
-            alert("Unable to connect to GitHub");
-        });
-
-
-};
-// document variables
-var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term")
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 
-var displayRepos = function (repos, searchTerm) {
-    console.log(repos);
-    console.log(searchTerm);
-    //if there is no repository
-    if (repos.length === 0) {
-        repoContainerEl.textContent = "No repositories found.";
-        return;
-    }
+var formSubmitHandler = function(event) {
+  // prevent page from refreshing
+  event.preventDefault();
+
+  // get value from input element
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+
+    // clear old content
     repoContainerEl.textContent = "";
-    repoSearchTerm.textContent = searchTerm;
-    //loop over repos
-    for (var i = 0; i < repos.length; i++) {
-        //formatting the repo name
-        var repoName = repos[i].owner.login + "/" + repos[i].name;
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
+};
 
-        //create container for each repo
-        var repoEl = document.createElement("a");
-        repoEl.classList = "list-item flex-row justify-space-between align-center";
-        repoEl.setAttribute("href", "./single-repo.html")
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-        //create a span element to hold repository name
-        var titleEl = document.createElement("span");
-        titleEl.textContent = repoName;
+  // make a get request to url
+  fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function(data) {
+          console.log(data);
+          displayRepos(data, user);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    })
+    .catch(function(error) {
+      alert("Unable to connect to GitHub");
+    });
+};
 
-        //append to container
-        repoEl.appendChild(titleEl);
+var displayRepos = function(repos, searchTerm) {
+  // check if api returned any repos
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
 
-        //create a status element
-        var statusEl = document.createElement("span");
-        statusEl.classList = "flex-row align-center";
+  repoSearchTerm.textContent = searchTerm;
 
-        if (repos[i].open_issues_count > 0) {
-            statusEl.innerHTML =
-                "<i class = 'fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-        } else {
-            statusEl.innerHTML = "<i class = 'fas fa-check-square status-icon icon-success'></i>";
-        }
+  // loop over repos
+  for (var i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-        //apend to container
-        repoEl.appendChild(statusEl);
+    // create a link for each repo
+    var repoEl = document.createElement("a");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
-        //append container to the DOM
-        repoContainerEl.appendChild(repoEl)
-    }
-}
+    // create a span element to hold repository name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
 
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-    var username = nameInputEl.value.trim();
-    if (username) {
-        getUserRepos(username);
-        nameInputEl.value = "";
+    // append to container
+    repoEl.appendChild(titleEl);
+
+    // create a status element
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+
+    // check if current repo has issues or not
+    if (repos[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
     } else {
-        alert("Please enter a GitHub username");
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+    // append to container
+    repoEl.appendChild(statusEl);
+
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+  }
+};
+var getFeatureRepos= function(language){
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    
+    fetch(apiUrl).then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                displayRepos(data.items, language)
+            });
+        }else {
+            alert('Error:GitHub User Not Found');
+        }
+    });
+};
+
+
+
+
+var buttonClickHandler=function(event){
+    //get language attribute from click
+    var language = event.target.getAttribute("data-language")
+    if (language){
+        getFeatureRepos(language);
+
+        //clear old content
+        repoContainerEl.textContent="";
+        
     }
 };
+// add event listeners to forms and buttons
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
+
